694a9e4963b3bfe24e478067ef6e7f526e5d0f71
Suhani Sharma <sus009@ucsd.edu>
2024-05-10 16:38:29 -0700

    test2 is the csv of the graph
    i messed around with everything until i could get the line graph to show  - test 2 is the good csv to use but feel free to make changes and save it as a new one just make sure to change that in the index.html too

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 <script>
 
 // set the dimensions and margins of the graph
-var margin = {top: 60, right: 30, bottom: 20, left:110},
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -14,74 +14,43 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//read data
-d3.csv("dsc106-project3/congressional_resignations.csv", function(data) {
+//Read the data
+d3.csv("CLEANED_CSVs/resignation.csv",
 
-  // Get the different categories and count them
-  //our data frame is being stored as data
-  var categories = data.columns
-  var n = categories.length
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([-10, 140])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+  // Now I can use this dataset:
+  function(data) {
 
-  // Create a Y scale for densities
-  var y = d3.scaleLinear()
-    .domain([0, 0.4])
-    .range([ height, 0]);
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  // Create the Y axis for names
-  var yName = d3.scaleBand()
-    .domain(categories)
-    .range([0, height])
-    .paddingInner(1)
-  svg.append("g")
-    .call(d3.axisLeft(yName));
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-  // Compute kernel density estimation for each column:
-  var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
-  var allDensity = []
-  for (i = 0; i < n; i++) {
-      key = categories[i]
-      density = kde( data.map(function(d){  return d[key]; }) )
-      allDensity.push({key: key, density: density})
-  }
-
-  // Add areas
-  svg.selectAll("areas")
-    .data(allDensity)
-    .enter()
-    .append("path")
-      .attr("transform", function(d){return("translate(0," + (yName(d.key)-height) +")" )})
-      .datum(function(d){return(d.density)})
-      .attr("fill", "#69b3a2")
-      .attr("stroke", "#000")
-      .attr("stroke-width", 1)
-      .attr("d",  d3.line()
-          .curve(d3.curveBasis)
-          .x(function(d) { return x(d[0]); })
-          .y(function(d) { return y(d[1]); })
-      )
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
 })
-
-// This is what I need to compute kernel density estimation
-function kernelDensityEstimator(kernel, X) {
-  return function(V) {
-    return X.map(function(x) {
-      return [x, d3.mean(V, function(v) { return kernel(x - v); })];
-    });
-  };
-}
-function kernelEpanechnikov(k) {
-  return function(v) {
-    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-  };
-}
 
 </script>
